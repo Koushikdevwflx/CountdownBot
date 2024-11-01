ad76d3a7a2510554d95a192cb03b96c7cbfdee22
Koushikdevwflx <koushikdevalla2005@gmail.com>
2024-11-01 16:00:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-import tweepy
 import os
 from datetime import date, timedelta
 from json import dumps
 
-# Print statements for debugging
-print("TWITTER_API_KEY:", os.environ.get("TWITTER_API_KEY"))
-print("TWITTER_API_SECRET_KEY:", os.environ.get("TWITTER_API_SECRET_KEY"))
-print("TWITTER_ACCESS_TOKEN:", os.environ.get("TWITTER_ACCESS_TOKEN"))
-print("TWITTER_ACCESS_TOKEN_SECRET:", os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"))
-
-# Initialize Twitter client
-consumer_key = os.environ['TWITTER_API_KEY']
-consumer_secret = os.environ['TWITTER_API_SECRET_KEY']
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
-client = tweepy.Client(
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token=access_token,
-    access_token_secret=access_token_secret
-)
-
-# Calculate countdown
-days = date(2024, 11, 27) - date.today() - timedelta(days=1)
-cd = dumps(days.days, default=str)
-print("Days countdown:", cd)  # Debug print statement
-
-# Create tweet
-response = client.create_tweet(text=cd)
-print("Tweet response:", response)  # Debug print statement
+try:
+    # Calculate countdown
+    days = date(2024, 9, 27) - date.today() - timedelta(days=1)
+    cd = dumps(days.days, default=str)
+    print("Days countdown:", cd)  # Print instead of tweeting
+except Exception as e:
+    print("An error occurred:", e)
